93b53281f5c369a4966c681b3ec212c061666a3c
Jared Usher <usherjared@yahoo.com>
2026-04-04 13:15:23 -0400

    Add more props to scene, randomized car and tree texture during instantiating

--- scenes/car.gd
+++ scenes/car.gd
@@ -3,8 +3,16 @@ extends Area2D
 var direction := Vector2.LEFT
 var speed := 100
 
+const GREEN = preload("uid://d0dya2slufckd")
+const RED = preload("uid://ocjfhrelwysu")
+const YELLOW = preload("uid://ded8huv3lr1a7")
+
+
+const CARS = [GREEN, RED, YELLOW]
 
 func _ready() -> void:
+	var random_car_num = randi_range(0, CARS.size()-1)
+	$Sprite2D.texture = CARS[random_car_num]
 	if position.x < 0:
 		direction.x = 1
 		$Sprite2D.flip_h = true
---
+++ scenes/tree.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+
+const TREE_2 = preload("uid://y3ndpd5bhsi3")
+const TREE_1 = preload("uid://d0so81ys3820n")
+
+const TREES = [TREE_1, TREE_2]
+
+
+func _ready():
+	var random_tree_num = randi_range(0, 1)
+	$TreeSprite.texture = TREES[random_tree_num]
